130a7df5e5052df1799da4911073c35b543739ac
Chase <cabbott93@gmail.com>
2021-06-12 14:48:34 -0700

    more file re org

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 import Home from '../components/home/Home';
 import About from '../components/about/About';
 import Resume from '../components/resume/Resume';
