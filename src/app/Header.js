a66724466151c6788875cde6aba2a8fe93497755
Chase <cabbott93@gmail.com>
2021-06-12 14:33:46 -0700

    grid formatting title and text

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
@@ -7,16 +7,21 @@ class Header extends Component {
   render() { 
     return (
       <header className="Header fixed w-full">
-        <nav className="grid grid-cols-2 content-between w-full">
+        <nav className="grid grid-cols-3 content-between w-full">
           <h2 className="cursor-pointer text-green-600 p-6 underline">
-            <Link to="/">
+            <NavLink to="/">
             Chase Abbott
-            </Link>
+            </NavLink>
+          </h2>
+          <h2 className="justify-self-center cursor-pointer text-green-600 p-6 underline">
+            <NavLink to="/resume">
+            Resume
+            </NavLink>
           </h2>
           <h2 className="cursor-pointer justify-self-end text-green-600 p-6 underline">
-            <Link to="/about">
+            <NavLink to="/about">
              About 
-            </Link>
+            </NavLink>
           </h2>
         </nav>
 
---
+++ src/components/home/resume/Resume.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Resume.css';
+
+export default class Resume extends Component {
+  
+  render() {
+    return (
+      <div className="Resume">
+        
+      </div>
+    );
+  }
+
+}
