fd7dfa0d1a63169b3fc571088494992db8008c98
Chase <cabbott93@gmail.com>
2021-06-23 16:08:59 -0700

    fix spacing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Banner extends Component {
-  
+
   render() {
     return (
-        <div className="Banner grid grid-cols-5 bg-opacity-5 bg-black p-4 mb-4 ">
-          <label className="flex justify-center ">
-            <img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg" alt="mongodb" className="h-4 tablet:w-auto tablet:h-auto"></img>
-          </label>        
-          <label className="flex justify-center">
-            <img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg" alt="node.js"></img>
-          </label>        
-          <label className="flex justify-center">
-            <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg" alt="react"></img>
-          </label>        
-          <label className="flex justify-center">
-            <img src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" alt="postgres"></img>
-          </label>        
-          <label className="flex justify-center">
-            <img src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" alt="express.js"></img>
-          </label>        
-        </div>
+      <div className="Banner grid grid-cols-5 bg-opacity-5 bg-black p-4 mb-4 ">
+        <label className="flex justify-center ">
+          <img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg" alt="mongodb" className="h-4 tablet:w-auto tablet:h-auto"></img>
+        </label>
+        <label className="flex justify-center">
+          <img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg" alt="node.js"></img>
+        </label>
+        <label className="flex justify-center">
+          <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg" alt="react"></img>
+        </label>
+        <label className="flex justify-center">
+          <img src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" alt="postgres"></img>
+        </label>
+        <label className="flex justify-center">
+          <img src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" alt="express.js"></img>
+        </label>
+      </div>
     );
   }
 
