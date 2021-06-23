57ce691a06a7ce257ab5bed323163af1ba6151a1
Chase <cabbott93@gmail.com>
2021-06-23 13:58:31 -0700

    fix spacing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,23 +15,23 @@ export default class Home extends Component {
     return (
       <div className="Home mt-32 w-full tablet:mt-60 font-theme">
         <Fade bottom cascade>
-        <div className="grid grid-rows-2 justify-center text-2xl w-full tablet:text-5xl desktop:text-7xl text-green-600 leading-none mb-8 font-bold" >
-          <span className="flex justify-self-start animate-wavey">FULL STACK</span> 
-          <span className="flex justify-self-start animate-fadein1_1s">SOFTWARE DEVLOPER</span> 
-          <span className="flex justify-self-start animate-fadein1_2s">BASED IN PORTLAND, OR</span>   
-        </div>
-        <div className=" grid grid-rows-2 text-green-600 tablet:mr-12 animate-fadein1_3s">
-          <p className="grid grid-cols-4 tablet:grid-cols-4 mb-4"> 
-            <span className="col-start-2 col-span-3 tablet:col-start-3 tablet:col-span-2 text-xs leading-none tablet:text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat cursus sodales. Nam nisi erat, ultricies id rhoncus rutrum, aliquet vel lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vitae erat sit amet nunc feugiat sodales a aliquet leo. Vivamus pulvinar blandit cursus. Donec id efficitur lectus, eu sagittis nibh. 
-            </span>
-          </p>
-          <p className="grid grid-cols-4 tablet:grid-cols-4 mb-4">
-            <span className="col-start-2 col-span-3 tablet:col-start-3 tablet:col-span-2 text-xs leading-none tablet:text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat cursus sodales. Nam nisi erat, ultricies id rhoncus rutrum, aliquet vel lorem.
-            </span>
-          </p>
-        </div>
+          <div className="grid grid-rows-2 justify-center text-2xl w-full tablet:text-5xl desktop:text-7xl text-green-600 leading-none mb-8 font-bold" >
+            <span className="flex justify-self-start animate-wavey">FULL STACK</span> 
+            <span className="flex justify-self-start animate-fadein1_1s">SOFTWARE DEVLOPER</span> 
+            <span className="flex justify-self-start animate-fadein1_2s">BASED IN PORTLAND, OR</span>   
+          </div>
+          <div className=" grid grid-rows-2 text-green-600 tablet:mr-12 animate-fadein1_3s">
+            <p className="grid grid-cols-4 tablet:grid-cols-4 mb-4"> 
+              <span className="col-start-2 col-span-3 tablet:col-start-3 tablet:col-span-2 text-xs leading-none tablet:text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat cursus sodales. Nam nisi erat, ultricies id rhoncus rutrum, aliquet vel lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vitae erat sit amet nunc feugiat sodales a aliquet leo. Vivamus pulvinar blandit cursus. Donec id efficitur lectus, eu sagittis nibh. 
+              </span>
+            </p>
+            <p className="grid grid-cols-4 tablet:grid-cols-4 mb-4">
+              <span className="col-start-2 col-span-3 tablet:col-start-3 tablet:col-span-2 text-xs leading-none tablet:text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat cursus sodales. Nam nisi erat, ultricies id rhoncus rutrum, aliquet vel lorem.
+              </span>
+            </p>
+          </div>
         </Fade>
         <Banner />
         <Projects align={'left'} data={data.hoops} />
