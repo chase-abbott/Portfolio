8ea4eedfab16761753561e1bf8059ed465e15baf
Chase <cabbott93@gmail.com>
2021-06-24 09:27:18 -0700

    start mods

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,11 @@ export default class Projects extends Component {
     return (
       <div>
         {(align === 'left') ?
-          <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid tablet:grid-cols-2 tablet:h-.5vh">
+          <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid tablet:h-2/4 tablet:relative">
             <a href={data.URL} className="flex justify-center items-center" target="_blank" rel="noreferrer">
-              <img src={data.image} className="tablet:w-3/4 tablet:h-3/4" alt={data.name}></img>
+              <img src={data.image} className="tablet:w-2/6 tablet:h-2/6" alt={data.name}></img>
             </a>
-            <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center flex-col tablet:absolute">
               <div className="grid grid-cols-projectLabel">
                 <label className="tablet:mt-6"> {data.label} </label>
                 <h3 className="desktop:text-6xl tablet:text-3xl"> {data.name} </h3>
@@ -19,8 +19,8 @@ export default class Projects extends Component {
               <span> {data.tools}</span>
             </div>
           </div>
-          : <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid grid-rows-projects grid-cols-1 mt-10 tablet:grid-cols-2 tablet:grid-rows-1 tablet:m-0 tablet:h-.5vh">
-            <div className="flex row-start-2 col-start-2 tablet:row-start-1 tablet:col-start-1 items-center justify-center flex-col">
+          : <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid grid-rows-projects grid-cols-1 mt-10 tablet:m-0 tablet:h-2/4 tablet:relative">
+            <div className="flex row-start-2 tablet:absolute items-center justify-center flex-col">
               <div className="grid grid-cols-projectLabel">
                 <label className="tablet:mt-6"> {data.label} </label>
                 <h3 className="desktop:text-6xl tablet:text-3xl"> {data.name} </h3>
@@ -28,7 +28,7 @@ export default class Projects extends Component {
               <span> {data.tools}</span>
             </div>
             <a href={data.URL} className="flex justify-center items-center col-start-2" target="_blank" rel="noreferrer">
-              <img src={data.image} className="tablet:w-3/4 tablet:h-3/4" alt={data.name}></img>
+              <img src={data.image} className="tablet:w-2/6 tablet:h-2/6" alt={data.name}></img>
             </a>
           </div>
         }
