cef6f3466961d6b13f3fb6d9507e325135af3669
Chase <cabbott93@gmail.com>
2021-06-24 09:55:04 -0700

    experimenting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,28 +7,28 @@ export default class Projects extends Component {
     return (
       <div>
         {(align === 'left') ?
-          <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid tablet:grid-cols-2 tablet:h-.5vh">
-            <a href={data.URL} className="flex justify-center items-center" target="_blank" rel="noreferrer">
-              <img src={data.image} className="tablet:w-3/4 tablet:h-3/4" alt={data.name}></img>
+          <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 tablet:h-.5vh tablet:relative">
+            <a href={data.URL} className="flex justify-center items-center tablet:self-center" target="_blank" rel="noreferrer">
+              <img src={data.image} className="tablet:w-3/6 tablet:h-3/6 tablet:mr-96" alt={data.name}></img>
             </a>
-            <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center flex-col tablet:absolute tablet:right-16 tablet:bottom-16 ">
               <div className="grid grid-cols-projectLabel">
                 <label className="tablet:mt-6"> {data.label} </label>
-                <h3 className="desktop:text-6xl tablet:text-3xl"> {data.name} </h3>
+                <h3 className="desktop:text-8xl tablet:text-3xl"> {data.name} </h3>
               </div>
               <span> {data.tools}</span>
             </div>
           </div>
-          : <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 grid grid-rows-projects grid-cols-1 mt-10 tablet:grid-cols-2 tablet:grid-rows-1 tablet:m-0 tablet:h-.5vh">
-            <div className="flex row-start-2 col-start-2 tablet:row-start-1 tablet:col-start-1 items-center justify-center flex-col">
+          : <div id={data.label} className="Projects text-xs tablet:text-base font-theme text-green-600 tablet:h-.5vh grid-rows-projects grid-cols-1 mt-10 tablet:mt-8 tablet:relative">
+            <div className="flex row-start-2 col-start-2 items-center justify-center flex-col tablet:absolute tablet:right-16 tablet:bottom-16">
               <div className="grid grid-cols-projectLabel">
                 <label className="tablet:mt-6"> {data.label} </label>
-                <h3 className="desktop:text-6xl tablet:text-3xl"> {data.name} </h3>
+                <h3 className="desktop:text-8xl tablet:text-3xl"> {data.name} </h3>
               </div>
               <span> {data.tools}</span>
             </div>
             <a href={data.URL} className="flex justify-center items-center col-start-2" target="_blank" rel="noreferrer">
-              <img src={data.image} className="tablet:w-3/4 tablet:h-3/4" alt={data.name}></img>
+              <img src={data.image} className="tablet:w-3/6 tablet:h-3/6" alt={data.name}></img>
             </a>
           </div>
         }
