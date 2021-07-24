0a027367af04f16f77c2bcefd6932567f45abf35
Chase <cabbott93@gmail.com>
2021-07-24 14:05:26 -0700

    remove dropdown from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 class Header extends Component {
-
-  handleMouseEnter = () => {
-    const ul = document.getElementById('nav-dropdown');
-    ul.classList.remove('hidden');
-  }
-
-  handleMouseLeave = () => {
-    const ul = document.getElementById('nav-dropdown');
-    ul.classList.add('hidden');
-  }
 
   scrollToProjects = async () => {
     const { history } = this.props;
@@ -34,18 +23,7 @@ class Header extends Component {
           <h2 className="justify-self-center cursor-pointer text-green-600 p-6 w-max" 
             onMouseEnter={this.handleMouseEnter} 
             onMouseLeave={this.handleMouseLeave}>
-            <NavLink to="/" className="hover:underline cursor-pointer">Chase Abbott</NavLink>
-            <ul id="nav-dropdown" className="hidden grid hover:grid-rows-3">
-              <li className="hover:underline w-max">
-                <h4 className="flex justify-self-center"> <FaLinkedin/> </h4>
-              </li>
-              <li className="hover:underline w-max">
-                <a href="https://github.com/chase-abbott"> 
-                  <h4> <FaGithub/> </h4>
-                </a>
-              </li>
-              <li className="hover:underline w-max" onClick={this.scrollToProjects}>Projects</li>
-            </ul>
+            <NavLink to="/" className="hover:underline cursor-pointer" onClick={this.scrollToProjects}>Chase Abbott</NavLink>
           </h2>
           <h2 className="justify-self-center cursor-pointer text-green-600 p-6 hover:underline">
             <NavLink to="/resume">
